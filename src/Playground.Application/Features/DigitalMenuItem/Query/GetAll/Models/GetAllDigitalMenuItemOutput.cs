e31a4546732c466694ab145a28441ce893512dd7
Albert Kellner Lescinskiene <albert.kellner@gmail.com>
2023-11-02 16:33:52 -0300

    feat: modelando pratos

--- src/Playground.Application/Features/DigitalMenuItem/Query/GetAll/Models/GetAllDigitalMenuItemOutput.cs
+++ src/Playground.Application/Features/DigitalMenuItem/Query/GetAll/Models/GetAllDigitalMenuItemOutput.cs
@@ -5,16 +5,46 @@ namespace Playground.Application.Features.DigitalMenuItem.Query.GetAll.Models
     public class GetAllDigitalMenuItemOutput
     {
         [JsonPropertyName("id")]
-        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid Id { get; set; }
 
         [JsonPropertyName("dish_name")]
         public string DishName { get; set; } = string.Empty;
 
+        [JsonPropertyName("description")]
+        public string Description { get; set; } = string.Empty;
+
         [JsonPropertyName("price")]
         public decimal Price { get; set; }
 
+        [JsonPropertyName("category")]
+        public string Category { get; set; } = string.Empty;
+
+        [JsonPropertyName("cooking_time")]
+        public int CookingTime { get; set; }
+
+        [JsonPropertyName("serving_size")]
+        public int ServingSize { get; set; }
+
+        [JsonPropertyName("ingredients")]
+        public string[] Ingredients { get; set; }
+
+        [JsonPropertyName("allergens")]
+        public string[] Allergens { get; set; }
+
+        [JsonPropertyName("spiciness_level")]
+        public string SpicinessLevel { get; set; } = string.Empty;
+
         [JsonPropertyName("is_available")]
         public bool IsAvailable { get; set; } = true;
+
+        [JsonPropertyName("image_url")]
+        public string ImageUrl { get; set; } = string.Empty;
+
+        [JsonPropertyName("chef_recommendation")]
+        public bool ChefRecommendation { get; set; } = false;
+
+        [JsonPropertyName("special")]
+        public bool Special { get; set; } = false;
 
         public bool IsValid() => true;
     }
