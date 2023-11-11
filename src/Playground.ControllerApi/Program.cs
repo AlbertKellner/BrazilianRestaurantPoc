2f062d5db50f39c3c0df6093e1ab3b904c40fd43
Albert Kellner Lescinskiene <albert.kellner@gmail.com>
2023-11-11 00:03:45 -0300

    AddSampleDishes

--- src/Playground.ControllerApi/Program.cs
+++ src/Playground.ControllerApi/Program.cs
@@ -1,3 +1,5 @@
+using BrazilianRestaurant.Application.Shared.InMemoryDatabase.DataBaseItem;
+using Playground.Application.Shared.InMemoryDatabase;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +18,8 @@ app.UseAuthorization();
 app.MapControllers();
 app.UseCors("AllowAll");
 
+AddSampleDishes();
+
 app.Run();
 
 FlushLogsBeforeCloseApplication();
@@ -26,4 +30,26 @@ FlushLogsBeforeCloseApplication();
 static void FlushLogsBeforeCloseApplication()
 {
     Log.CloseAndFlush();
+}
+
+void AddSampleDishes()
+{
+    var dishes = new List<DataBaseDishItem>
+        {
+            new DataBaseDishItem { Id = Guid.NewGuid(), DishName = "Feijoada", Price = 20, ChefRecommendation = true },
+            new DataBaseDishItem { Id = Guid.NewGuid(), DishName = "Pão de Queijo", Price = 5, ChefRecommendation = false },
+            new DataBaseDishItem { Id = Guid.NewGuid(), DishName = "Moqueca", Price = 22, ChefRecommendation = true },
+            new DataBaseDishItem { Id = Guid.NewGuid(), DishName = "Churrasco", Price = 25, ChefRecommendation = true },
+            new DataBaseDishItem { Id = Guid.NewGuid(), DishName = "Coxinha", Price = 4, ChefRecommendation = false },
+            new DataBaseDishItem { Id = Guid.NewGuid(), DishName = "Acarajé", Price = 6, ChefRecommendation = false },
+            new DataBaseDishItem { Id = Guid.NewGuid(), DishName = "Brigadeiro", Price = 3, ChefRecommendation = false },
+            new DataBaseDishItem { Id = Guid.NewGuid(), DishName = "Pastel", Price = 5, ChefRecommendation = false },
+            new DataBaseDishItem { Id = Guid.NewGuid(), DishName = "Tapioca", Price = 7, ChefRecommendation = false },
+            new DataBaseDishItem { Id = Guid.NewGuid(), DishName = "Empadão", Price = 8, ChefRecommendation = false }
+        };
+
+    foreach (var dish in dishes)
+    {
+        DishInMemoryDatabase.Instance.AddDishItem(dish);
+    }
 }
